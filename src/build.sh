20c3b3f0d2d379ea905b47eacafcc683cd5d691d
shastro <guitarshughes@hotmail.com>
2023-10-22 19:50:56 -0600

    Add debug flag to build script

--- src/build.sh
+++ src/build.sh
@@ -2,6 +2,6 @@
 pushd "$(dirname "$0")"
 
 mkdir -p ../build
-cc ../src/template_raylib.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o ../build/template_raylib
+cc ../src/template_raylib.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -g -o ../build/template_raylib
 
 popd
