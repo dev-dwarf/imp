0b92f5697745ab7b10819ff023a560d6d24d49e7
lf <contact@loganforman.com>
2025-10-26 23:09:07 -0600

    switch font to inter

--- main.c
+++ main.c
@@ -11,16 +11,18 @@ static SDL_Surface *atlas_surface = 0;
 static SDL_Texture *atlas_texture = 0; 
 
 #define ARRAY_LENGTH(A) (sizeof(A)/sizeof(*(A)))
+#define ASSERT_SDL(cond) do { if (!(cond)) { SDL_Log( "" __FILE__ ":%d: " #cond " gave SDL Error: %s", __LINE__, SDL_GetError()); return -1; } } while(0);
 
 
 #define WIDTH 640
 #define HEIGHT 480
 
-#define ASSERT_SDL(cond) do { if (!(cond)) { SDL_Log( "" __FILE__ ":%d: " #cond " gave SDL Error: %s", __LINE__, SDL_GetError()); return -1; } } while(0);
+
+
 
 int main(int argc, char* argv[]) {
     ASSERT_SDL(SDL_Init(SDL_INIT_VIDEO) >= 0);
-    ASSERT_SDL(window = SDL_CreateWindow("HelloWorld SDL3", WIDTH, HEIGHT, 0));
+    ASSERT_SDL(window = SDL_CreateWindow("IMP SDL3", WIDTH, HEIGHT, 0));
     ASSERT_SDL(renderer = SDL_CreateRenderer(window, NULL));
     
     SDL_Event event;
@@ -34,7 +36,7 @@ int main(int argc, char* argv[]) {
     SDL_Palette *atlas_palette ;
     ASSERT_SDL(atlas_surface = SDL_CreateSurfaceFrom(ATLAS_WIDTH, ATLAS_HEIGHT, SDL_PIXELFORMAT_INDEX8, ATLAS_DATA, ATLAS_WIDTH));
     ASSERT_SDL(atlas_palette = SDL_CreatePalette(256));
-    for (int i = 0; i < 256; i++) { atlas_palette->colors[i] = (SDL_Color) { i, i, i, 255}; }
+    for (int i = 0; i < 256; i++) { atlas_palette->colors[i] = (SDL_Color) { i, i, i, i}; }
     ASSERT_SDL(SDL_SetSurfacePalette(atlas_surface, atlas_palette));
     ASSERT_SDL(atlas_texture = SDL_CreateTextureFromSurface(renderer, atlas_surface));
 
@@ -55,44 +57,15 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        float height = HEIGHT;
-        float width = WIDTH;
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderClear(renderer);
-
-        SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
-        // SDL_RenderLine(renderer, 50, height / 2, width - 50, height / 2);
-        // SDL_RenderLine(renderer, 50, 50, 50, height - 50);
-
-        // for (int x = 50; x <= width - 50; x += 50) {
-        //     SDL_RenderLine(renderer, x, height / 2 - 5, x, height / 2 + 5);
-        // }
-        // for (int y = 50; y <= height - 50; y += 50) {
-        //     SDL_RenderLine(renderer, 45, y, 55, y);
-        // }
-
-        // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        // SDL_FPoint points[360];
-        
-        // for (int x = 0; x < 360; x++) {
-        //     float angle = 4 * x * M_PI / 180.0f;
-        //     float y = sin(angle) * ((height - 100) / 4.0f);
-        //     points[x].x = 50 + x * ((width - 100) / 360.0f);
-        //     points[x].y = (height / 2.0f) - y;
-        // }
-        
-        // SDL_RenderLines(renderer, points, 360);
-
-        // SDL_RenderFillRect(renderer, &mouseRect);
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
         SDL_FRect r = {0, 0, ATLAS_WIDTH, ATLAS_HEIGHT};
         SDL_RenderTexture(renderer, atlas_texture, &r, &r);
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderRects(renderer, ATLAS_RECT, ARRAY_LENGTH(ATLAS_RECT));
 
         SDL_RenderPresent(renderer);
