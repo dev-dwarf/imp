129773d55ea736037f123577859a1d1268369217
lf <contact@loganforman.com>
2025-10-30 22:07:24 -0600

    change flake to run any package

--- flake.nix
+++ flake.nix
@@ -1,27 +1,26 @@
 {
-  description = "Template for a plotting program using SDL3 on NixOS";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";  # Adjust if needed for other architectures
+    system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    deps = {
-      buildInputs = with pkgs; [ sdl3 ];
-    };
   in {
-    packages.${system}.default = pkgs.stdenv.mkDerivation (deps // {
-      pname = "imp-sdl3";
-      version = "0.0.0";
-      src = ./.;
-      allowSubstitutes = false;
-      dontStrip = true;
-      buildPhase = ''
-        mkdir -p $out/bin
-        ${pkgs.stdenv.cc}/bin/cc main.c -o $out/bin/imp-sdl3 -lSDL3 -lm -g -Og
-      '';
-    });
+    packages.${system} = pkgs // rec {
+      imp-sdl3 = pkgs.stdenv.mkDerivation ({
+        pname = "imp-sdl3";
+        version = "0.0.0";
+        src = ./.;
+        buildInputs = [ pkgs.sdl3 ];
+        buildPhase = ''
+          mkdir -p $out/bin
+          ${pkgs.stdenv.cc}/bin/cc main.c -o $out/bin/imp-sdl3 -lSDL3 -lm
+        '';
+      });
+
+      default = imp-sdl3;
+    };
   };
 }
+
